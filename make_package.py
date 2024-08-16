551683212dfbb482eacaa2ebf7f3bfeca4bb7e22
James Endicott <endicott.james@gmail.com>
2024-08-15 21:46:23 -0500

    Initial version of script to make an importable archive.

---
+++ make_package.py
@@ -0,0 +1,76 @@
+import os
+import shutil
+import json
+import re
+from datetime import datetime
+
+CATEGORY = "neogob"
+EMOJI_FILE_TYPES = (".gif", ".png", ".apng", ".webp")
+HOST = "sakurajima.social"
+LICENSE = "CC BY-NC-SA 4.0"
+
+name_pattern = re.compile(r"neogob_?(.*)\.(png)")
+
+def clean_name(file_name):
+    emoji_name = name_pattern.match(file_name).groups()[0]
+    match(emoji_name):
+        case "0_0":
+            return "neogob 0_0"
+        case "x_x":
+            return "neogob x_x"
+        case "_":
+            return "neogob __"
+        case "_w_":
+            return "neogob _w_"
+        case "":
+            return "neogob"
+        case _:
+            return f"neogob {emoji_name.replace("_", " ")}"
+
+def make_emoji_json():
+    emoji_list = []
+    for emoji in os.scandir("neogob"):
+        if emoji.name.endswith(EMOJI_FILE_TYPES):
+            emoji = {
+                "downloaded": True,
+                "fileName": f"{emoji.name}",
+                "emoji": {
+                    "name": clean_name(emoji.name),
+                    "category": CATEGORY,
+                    "license": LICENSE,
+                    "aliases": []
+                }
+            }
+            emoji_list.append(emoji)
+    data = {
+        "metaVersion": 2,
+        "host": HOST,
+        "exportedAt": datetime.now().strftime("%Y-%m-%dT%H:%I:%SZ"),
+        "emojis": emoji_list
+    }
+    with open(f"neogob/meta.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+def make_pack_json():
+    emoji_dict = {}
+    for emoji in os.scandir("neogob"):
+        if emoji.name.endswith(EMOJI_FILE_TYPES):
+            emoji_dict[clean_name(emoji.name)] = f"{emoji.name}"
+    data = {
+        "files": emoji_dict,
+        "pack": {},
+        "count": len(emoji_dict)
+    }
+    with open(f"neogob/pack.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+if __name__ == "__main__":
+    make_emoji_json()
+    make_pack_json()
+    shutil.copy("LICENSE", f"neogob\\LICENSE")
+    shutil.copy("THANKS", f"neogob\\THANKS")
+    shutil.make_archive("neogob", "zip", "neogob")
+    os.remove("neogob\\LICENSE")
+    os.remove("neogob\\THANKS")
+    os.remove("neogob\\pack.json")
+    os.remove("neogob\\meta.json")
